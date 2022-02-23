a90b356a1d8d7ccd8a9fd375ae0da23219d28f6a
Valkyrux <philadephos.granata@gmail.com>
2022-02-23 16:45:22 +0100

    make_model_all

---
+++ database/migrations/2022_02_23_144704_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->char('title', 50);
+            $table->string('image')->nullable();
+            $table->tinyInteger('edition',);
+            $table->string('authors');
+            $table->string('artists');
+            $table->text('description')->nullable();
+            $table->decimal('price', 4, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Comic;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 4; $i++) {
+            $a_comic = new Comic();
+            $array_fill = [
+                'title' => $faker->word(),
+                'image' => $faker->imageUrl(640, 480, 'comic', true),
+                'edition' => $faker->numberBetween(1, 5),
+                'authors' => implode(', ', $faker->words($faker->numberBetween(1, 4))),
+                'artists' => implode(', ', $faker->words($faker->numberBetween(1, 4))),
+                'description' => $faker->paragraph($faker->numberBetween(3, 7)),
+                'price' => $faker->randomFloat(2, 5, 30),
+            ];
+            $a_comic->fill($array_fill);
+            $a_comic->save();
+        }
+    }
+}
